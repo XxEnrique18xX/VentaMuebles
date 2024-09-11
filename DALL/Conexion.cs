b0f90078849d99bd0ad12892f8e927c2ddd3cb1f
XxEnrique18xX <enrique.armando224@gmail.com>
2024-09-11 03:18:04 -0600

    Add project files.

---
+++ DALL/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Conexion
+    {
+        private IDbConnection conexion;
+        public IDbConnection Cadena()
+        {
+            return new SqlConnection("Server=DESKTOP-C0B4DJH\\SQLEXPRESS; Initial Catalog=VentaMuebles; Persist Security Info=True; Encrypt=false; User ID=sa;Password=database");
+        }
+
+    }
+}
